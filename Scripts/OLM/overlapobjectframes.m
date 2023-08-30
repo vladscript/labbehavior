60bc0a377307b3f857b778969e199787669b6d81
vladscript <melezio@gmail.com>
2023-08-29 18:31:28 -0600

    OLM updated overlapping body bugs 2

--- Scripts/OLM/overlapobjectframes.m
+++ Scripts/OLM/overlapobjectframes.m
@@ -3,27 +3,29 @@ NefFrames=find(dX<=0);
 OL=[];
 if ~isempty(NefFrames)
     Intervals=[1,find(diff(NefFrames)>1)+1,numel(NefFrames)];
-    
-    for n=1:numel(Intervals)-1
-        A=NefFrames(Intervals(n));
-        B=NefFrames(Intervals(n+1)-1);
-        % search backwards
-        while dX(A)<Dclose && A>1
-            A=A-1;
-            fprintf('.');
+    if numel(unique(Intervals))>1
+        for n=1:numel(Intervals)-1
+            A=NefFrames(Intervals(n));
+            B=NefFrames(Intervals(n+1)-1);
+            % search backwards
+            while dX(A)<Dclose && A>1
+                A=A-1;
+                fprintf('.');
+            end
+            fprintf('\n');
+            % search forwards
+            while dX(B)<Dclose && B<numel(dX)
+                B=B+1;
+                fprintf('.');
+            end
+            fprintf('\n');
+            OL=[OL;A,B,B-A];
         end
         fprintf('\n');
-        % search forwards
-        while dX(B)<Dclose && B<numel(dX)
-            B=B+1;
-            fprintf('.');
-        end
-        fprintf('\n');
-        OL=[OL;A,B,B-A];
+        [~,idxok]=unique(OL(:,1:2),'rows');
+        OL=OL(idxok,:);
     end
-    fprintf('\n');
-    [~,idxok]=unique(OL(:,1:2),'rows');
-    OL=OL(idxok,:);
+    
 else
     fprintf('\n No object overlapping \n');
 end
