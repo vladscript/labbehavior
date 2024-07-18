103ea346790909770c4cd2dbc24941cd756b7bc0
vladscript <melezio@gmail.com>
2024-07-17 20:48:19 -0600

    Vocal Raster Up

---
+++ Scripts/Sounds/Plot_Raster.m
@@ -0,0 +1,163 @@
+%% Function to plot raster
+% input
+%   R:   Matrix  Rows x Columns
+%   varargin default:
+%           fs=1            Bin Window
+%           stepy=2;        Step between rows Yticks
+%           indexes=1:C;    Sort
+%           ColocateIndx=[];
+%   Neurons:Colocalized
+% Output
+% figure of the raster in MINUTES
+% Always creates a new figure
+function Plot_Raster(R,varargin)
+%% Setup
+% Check Raster Range of Values
+vals=sort(unique(R(:)));
+if numel(vals)==2 && (and(vals(1)==0,vals(2)==1))
+    fprintf('\n>Binary mode\n')
+    CM=gray(2);
+    bincol=true;
+else
+    fprintf('\n>Colored mode\n');
+    bincol=false;
+    if sum(vals-round(vals))==0 % INTEGERS
+        notmultiply=true;
+        strcblabel='N';
+        bines=vals;
+        CM=gray(numel(bines)*2);
+        CM=CM(end:-2:1,:);
+    else
+        notmultiply=false;
+        [Nc,bines]=histcounts(R(R(:)>0));
+        CM=gray(numel(Nc)*2+1);
+        CM=CM(end-1:-2:1,:);
+        strcblabel='[ms]';
+    end
+end
+% Colormap
+
+TransientHight=0.75;
+%ColColor=[1,0.6,0.78]; % PINK
+N=numel(varargin);
+[C,~]=size(R);
+indexes=1:C;
+
+switch N
+    case 0
+        bins=1; % Discrete Time (frames)
+        stepy=1;
+        xstring='bins';
+    case 1
+        bins=varargin{1};
+        stepy=1;        
+        xstring='t [s]';
+    case 2
+        bins=varargin{1};
+        xstring='t [s]';
+        stepy=varargin{2};
+    otherwise
+        fprintf('\n Unrecognized #input parameters\n')
+end
+% ts=1/bins;
+%% Create Figure   ********************************************************
+figure
+ax1=subplot(1,1,1);
+hold on;
+%% MAIN LOOP ##########################################################
+for i=1:C
+    fprintf('%i: ',i)
+    ypositon=[i-TransientHight/2,TransientHight];
+    activeframes=find(R(i,:));
+    activeframes=[activeframes,0];
+    % never and active frame is going to be Zero
+    if ~isempty(activeframes(activeframes>0))
+        nf=1;
+        xposition(1)=activeframes(1);
+        xposition(2)=1;
+        val=R(i,activeframes(1));
+        
+        while nf<numel(activeframes)
+            nx=nf; 
+%             while activeframes(nx+1)==activeframes(nx)+1 % consecutive
+%                 xposition(2)=xposition(2)+1;
+%                 if nx+1==numel(activeframes)
+%                     % activeframes=[activeframes,0];
+%                     % never and active frame is going to be Zero
+%                     % Stop sloop;
+%                 else
+%                     nx=nx+1;
+%                     % SUM OF N AND LENGHTS
+%                 end
+%             end
+            % Create Rectangle *************************
+            xposs=xposition;
+            if bincol
+                % If there is Colocalized Cells
+                colorspike=CM(1,:);
+            else
+                ncolors=find(val>=bines);
+                if ~isempty(ncolors)
+                    ncolor=ncolors(end);
+                else
+                    ncolor=numel(bines)-1;
+                end
+                colorspike=CM(ncolor,:);
+            end
+            rectangle('Position',[xposs(1),ypositon(1),...
+                    xposs(2),ypositon(2)],'Curvature',[0,0],...
+                    'EdgeColor',colorspike,...
+                    'FaceColor',colorspike);
+            fprintf('|')
+            % Restart xposition values
+            % xposition(1)=activeframes(nx+1)-0.5;
+            xposition(1)=activeframes(nx+1);
+            xposition(2)=1;
+            if nx<numel(activeframes)-1
+                val=R(i,activeframes(nx+1));
+            end
+            nf=nx+1;    
+        end
+        fprintf('\n')
+    else
+        fprintf('\n')
+    end
+end
+
+if C>0
+    axis([0,size(R,2)+1,1-TransientHight/2,C+TransientHight/2])
+end
+    
+ylabel('Mouse/Segments')
+xlabel(xstring)
+set(ax1,'Box','off')
+yticks=indexes';
+set(ax1,'YTick',1:stepy:C)
+set(ax1,'YTickLabel',yticks(1:stepy:C))
+set(ax1,'TickLength',[0,0])
+
+if N>0
+    for j=1:numel(ax1.XTick)
+        ax1.XTickLabel{j}=num2str(ax1.XTick(j)*bins);
+    end
+end
+
+if ~bincol                
+    colormap(CM);
+    colorbar;
+    cb=colorbar;
+    cbTicks=linspace(cb.Limits(1),cb.Limits(end),numel(bines));
+    cb.Ticks=cbTicks;
+    for i=1:numel(bines)
+        if ~notmultiply
+            cb.TickLabels{i}=num2str(bines(i)*1000);
+        else
+            cb.TickLabels{i}=num2str(bines(i));
+        end
+    end
+    % fprintf('-'); 
+    titlecb=get(cb,'Title');
+    set(titlecb,'String',strcblabel)
+end
+
+end
---
+++ Scripts/Sounds/VocalRaster.m
@@ -0,0 +1,113 @@
+%% MAKE VOCAL - RASTERs
+% 
+% IT WORKS AT 2 MODES:
+%   > Single File:    rows: intervals (user-defined "trials")
+%   > Multiple Files: rows: files
+% 
+% Solved Issue: vocals are way too short so raster columns are
+% bins of time and whose values are counts of vocalization (binary) and how long (color)
+% 
+% Input
+%   Output files from DeepSqueck (xlsx)
+%   User Interface:
+%     Audio Sampling Frequency
+%     Length of Audio in s
+% Output (\outputCSVs)
+%   Audio Times of volcalizations (CSV)
+%   Binary Binned Raster Matrix (CSV)
+%   Length Binned Raster Matrix (CSV)
+% 
+clear; clc;
+% Read multiple file in a folder
+[A,C]=uigetfile('*.xlsx','MultiSelect','on');
+if ischar(A)
+    N=1;
+    Archs{1}=A;
+    fprintf('\n>Single File Mode\n')
+else
+    N=numel(A);
+    Archs=A;
+    fprintf('\n>Multiple File Mode N=%i\n',N);
+end
+%% User Input
+% Sampling [Hz]
+prompt = {'Sampling Frequency [kHz]'};
+dlgtitle = 'fs';
+dims = [1 30];
+definput = {'340'};
+fsinput = inputdlg(prompt,dlgtitle,dims,definput);
+fs=str2double(fsinput)*1000;
+% Length Recording [s]
+prompt = {'Length recording [minutes]'};
+dlgtitle = 'min';
+dims = [1 35];
+definput = {'5'};
+Linput = inputdlg(prompt,dlgtitle,dims,definput);
+L=str2double(Linput)*60;
+if N==1
+    % Multiple Records Raster Width [secs]
+    prompt = {'Raster  Trials/Segments [seconds]'};
+    dlgtitle = 's';
+    dims = [1 40];
+    definput = {'60'};
+    COLNUMinput = inputdlg(prompt,dlgtitle,dims,definput);
+    colnum=str2double(COLNUMinput);
+end
+% Window Raster [samples]
+prompt = {'Raster bin length [s]'};
+dlgtitle = 's';
+dims = [1 45];
+definput = {'0.5'};
+COLNUMinput = inputdlg(prompt,dlgtitle,dims,definput);
+w=str2double(COLNUMinput)*fs;
+% w=Ns*fs;
+
+%% Main Loop
+if N>1
+    Vall=zeros(N,L*fs/w);
+    Nall=zeros(N,L*fs/w);
+    Lall=zeros(N,L*fs/w);
+end
+for n=1:N
+    fprintf('\n>Loading %s, ',Archs{n})
+    DATA=readtable([C,Archs{n}]);
+    fprintf('done.\n, ')
+    % Use Begin & End Times
+    Ta=DATA.BeginTime_s_;
+    Tb=DATA.EndTime_s_;
+    % Creat zero vectors 
+    Dbin=zeros(1,L*fs/w); % Length of vocalization(s)
+    Vbin=zeros(1,L*fs/w); % Binary (there was or not vocalizations)
+    Nbin=zeros(1,L*fs/w); % Number of vocalizations
+    % Vocalizations Loop
+    for m=1:numel(Ta)
+        fprintf('\n>Vocal #%i',m)
+        a=round(Ta(m)*fs);
+        b=round(Tb(m)*fs);
+        l=DATA.CallLength_s_(m);        % length
+        % V(a:b)=1;
+        Vbin(floor(a/w):floor(b/w))=1;              % If vocalization(s)
+        Nbin(floor(a/w):floor(b/w))=Nbin(floor(a/w):floor(b/w))+1;  % N vocs
+        Dbin(floor(a/w):floor(b/w))=Dbin(floor(a/w):floor(b/w))+l;  % Length
+    end
+    if N>1
+        Vall(n,:)=Vbin;
+        Nall(n,:)=Nbin;
+        Lall(n,:)=Dbin;
+    end
+end
+%% Outputs
+if N==1
+    Vraster=reshape(Vbin,colnum,[])';
+    Draster=reshape(Dbin,colnum,[])';
+    Nraster=reshape(Nbin,colnum,[])';
+    Plot_Raster(Vraster,w/fs)
+    Plot_Raster(Draster,w/fs)
+    Plot_Raster(Nraster,w/fs)
+else
+    Plot_Raster(Vall,w/fs)
+    Plot_Raster(Lall,w/fs)
+    Plot_Raster(Nall,w/fs)
+    
+    
+end
